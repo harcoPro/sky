f8909cedf9445657da377bb5d723a7a7614725ca
shpakovskiy.k.a <shpakovsky.k.a@gmail.com>
2021-05-19 20:29:54 +0600

    Инициализация приложения

---
+++ app/src/main/java/com/shpakovsky/ka/myapplication/SkyApplication.kt
@@ -0,0 +1,10 @@
+package com.shpakovsky.ka.myapplication
+
+import android.app.Application
+
+
+class SkyApplication : Application() {
+
+
+
+}
---
+++ app/src/main/java/com/shpakovsky/ka/myapplication/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shpakovsky.ka.myapplication.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shpakovsky.ka.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
